6b1fe2ed56b55089ab5619e55871283bb874d186
Menga wanji <martialwanji@gmail.com>
2025-06-09 01:43:41 +0100

    done

--- script.js
+++ script.js
@@ -1,97 +1,69 @@
-const userInput = document.getElementById('gitName');
-const infoBox = document.querySelector(".info-box");
-const errorMessage = document.getElementById('userError');
 
+const input = document.querySelector('input')
+const btn = document.querySelector('button')
+const card = document.querySelector('.card')
 
- 
-let getProfile =(username) => {
-    const apiUrl =`https://api.github.com/users/${username}`;
+const repos_container = document.querySelector('.repos')
 
-    let isValid = true;
-    errorMessage.textContent = "";
-    
-    if (!userInput) {
-        errorMessage.textContent = "Please enter a valid Github Username";
-        isValid = false;    
-        return;  
-    }
-
-     fetch(apiUrl).then((res) => res.json()).then(data => { 
-
-        if(data.Response = 'True') {
-            const dateData = data.created_at.slice(0, data.created_at.length - 10);
-            const location = data.location === "" || data.location === null ? "No location" : data.location; 
-            const twitter = data.twitter_username === "" || data.twitter_username === null ? "No X" : data.twitter_username; 
-            const website = data.blog === "" || data.blog === null ? "No website" : data.blog; 
-            const company = data.company === "" || date.company === null ? "No company" : data.company; 
-            const bio = data.bio === "" || data.bio === null? "This profile has no bio" : data.bio;
-
-
-            infoBox.innerHTML = `
-
-            <div class="user-details"> 
-                <div class="img-box">
-                    <img src="${data.avatar_url}" alt="">
-                </div>
-                <div class="details">
-                    <h3 class="name"> ${data.name}</h3>
-                    <h3 class="username"> ${data.login}</h3>
-                    <span class="join-date">${dateData }</span>
-                </div>
-                <p class="bio">${data.bio}</p>
-                <div class="user-profile">
-                    <div class="repos">
-                        <h2>${data.public_repos}</h2> 
-                        <span>Repos</span>
-                    </div>
-                    <div class="followers">   
-                        <h2> ${data.followers}</h2>
-                        <span>Followers</span>
-                    </div>
-                    <div class="following">
-                        <h2> ${data.following}</h2>
-                        <span>following</span>
-                    </div>   
-                </div>
-                <div class="user-other-details">
-                    <p><i class="fa-solid fa-building"></i> ${company}</p>
-                    <p><i class="fa-solid fa-location-pin"></i> ${location}</p>
-                    <p><i class="fa-solid fa-link"></i>${website}</p>
-                    <p><i class="fa-brands fa-x-twitter"></i> ${twitter}</p>
-                </div>
-            </div>  `;
-
-        }
-    })
-        .catch(error => {
-        console.log('Error fetching user profile;', error);
-        errorMessage.textContent = "Please enter a valid username";
-        isValid = false;
-        return;
-    })
-
+async function user (username) {
+    const resp = await fetch(`https://api.github.com/users/${username}`)
+    const respData = await resp.json()
+    return respData
 }
 
-getProfile("github");
+repos('prodev2004')
+async function repos (username) {
+    const resp = await fetch(`https://api.github.com/users/${username}/repos`)
+    const respData = await resp.json()
 
+    return respData
+}
+async function add_repo () {
+    const reposData = await repos(input.value)
+    console.log(reposData);
+    repos_container.innerHTML = reposData.map(repo => {
+        return `
+            <div class="card">
+                <h2>${repo.name}</h2>
+                <a href="${repo.clone_url}" target="_blank">Take a look at this repo ></a>
+            </div>
+        `
+    }).join('')
+}
 
+btn.addEventListener('click', async () => {
+    const input_val = input.value
+    const search_result = await user(input_val)
 
+    add_repo()
 
-
-
-userInput.addEventListener('keyup', function(e) {
-    if (e.key == "Enter") {
-        getProfile();
+    if (!search_result.login) {
+        alert('No user found!')
+    } else {
+        card.innerHTML = `
+            <div class="avatar">
+                <img src="${search_result.avatar_url}" alt="">
+            </div>
+            <div class="info">
+                <h2>${search_result.name}</h2>
+                <p>${search_result.login}</p>
+                <div class="follow-info">
+                    <div class="single">
+                        <span>${search_result.followers}</span>
+                        <span>Followers</span>
+                    </div>
+                    <div class="single">
+                        <span>${search_result.following}</span>
+                        <span>Following</span>
+                    </div>
+                </div>
+                <div class="single">
+                    <span>${search_result.public_repos}</span>
+                    <span>Repos</span>
+                </div>
+            </div>
+            <a href="${search_result.html_url}" target="_blank">Visit Github Profile ></a>
+        `
     }
-});
- 
-
-
-
-
-
-
-
-
-
+})
 
